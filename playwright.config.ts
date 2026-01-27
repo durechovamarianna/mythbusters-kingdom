f672291596aaa1756581a1664e021d8ed129acff
Durechova Marianna <durechova@intra.ditec.sk>
2026-01-27 19:29:50 +0100

    Fix GitHub Actions folder name (.github)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
 
+  // Stabilita v CI
+  workers: 1,
+  fullyParallel: false,
   retries: 1,
 
   use: {
